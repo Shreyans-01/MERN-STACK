3ff8f7d5fb58e6999251985f7b2f14e648e8e698
Shreyans Mehta <shreyans.mehta@somaiya.edu>
2021-09-01 14:24:47 +0530

    Comments

--- 01-09/inheritance.js
+++ 01-09/inheritance.js
@@ -1,14 +1,18 @@
 //inheritance cooncept
 const EventEmitter = require('events')
 
+//util module
 var util = require('util')
 
+//class
 var Students = function(name){
     this.name = name;
 }
 
+//arguments class which inherits and objects of eventemitter
 util.inherits(Students,EventEmitter);
 
+//object of class 
 var std = new Students('Shreyans');
 
 std.on('Info', function(age){
